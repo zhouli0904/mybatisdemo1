0c56b24f2d32cb742ef51756dda9431d20385e1b
zhouli <zhouli@163.com>
2021-05-13 11:28:01 +0800

    添加test04

---
+++ src/test/java/com/springboot/mybatisdemo/Test04.java
@@ -0,0 +1,11 @@
+package com.springboot.mybatisdemo;
+
+import org.junit.jupiter.api.Test;
+
+public class Test04 {
+
+    @Test
+    public void test04(){
+        System.out.println("hhhh");
+    }
+}
